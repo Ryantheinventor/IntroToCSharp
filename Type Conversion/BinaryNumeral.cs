2a67a76d0de4eabba02381e3412a457a6ec697b9
Ryan Millen <ryantheinventor@gmail.com>
2020-08-27 10:45:54 -0700

    Added Type Conversion

---
+++ Type Conversion/BinaryNumeral.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Type_Conversion
+{
+    class BinaryNumeral
+    {
+        private int value;
+        public BinaryNumeral(int value)
+        {
+            
+            this.value = value;
+        }
+        // Declare a conversion from an int to a BinaryNumeral. Note the
+        // the use of the operator keyword. This is a conversion
+        // operator named RomanNumeral:
+        static public implicit operator BinaryNumeral(int value)
+        {
+            // Note that because BinaryNumeral is declared as a struct,
+            // calling new on the struct merely calls the constructor
+            // rather than allocating an object on the heap:
+            return new BinaryNumeral(value);
+        }
+        // Declare an explicit conversion from a BinaryNumeral to an int:
+        static public explicit operator int(BinaryNumeral binary)
+        {
+            return binary.value;
+        }
+        // Declare an implicit conversion from a BinaryNumeral to
+        // a string:
+        static public implicit operator string(BinaryNumeral binary)
+        {
+            return binary.value.ToString();
+        }
+    }
+}
---
+++ Type Conversion/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Type_Conversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RomanNumeral roman;
+            roman = 10;
+            BinaryNumeral binary;
+            // Perform a conversion from a RomanNumeral to a
+            // BinaryNumeral:
+            binary = (BinaryNumeral)(int)roman;
+            // Performs a conversion from a BinaryNumeral to a RomanNumeral.
+            // No cast is required:
+            roman = binary;
+            Console.WriteLine((int)binary);
+            Console.WriteLine(binary);
+            Console.ReadKey();
+        }
+    }
+}
